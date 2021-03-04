c51a627c135f35e2a6a53d87b7dd4d21f77549ee
Ivan Ugarte <ivanugartem@gmail.com>
2021-03-03 23:05:08 -0300

    update 03032021

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Layout from '../components/Layout'
-import { skills } from '../profile'
+import { skills, experiences, projects } from '../profile'
+import Link from 'next/link'
 
 const Index = () => (
   <Layout>
@@ -8,15 +9,16 @@ const Index = () => (
         <div className='card card-body bg-secondary text-light'>
           <div className='row'>
             <div className='col-md-4'>
-              <img src='foto.jpg' alt='' className='img-fluid' />
+              <img src='ivan1.jpg' alt='' className='img-fluid' />
             </div>
             <div className='col-md-8'>
               <h1>Iván Ugarte</h1>
               <h3>Analista de Sistemas</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero
-                ad id aut in architecto illum reprehenderit rerum minima tempora
-                vitae.
+                Analista de Sistemas Titulado en proceso de datos, experiencia
+                de 15 años en Main Frame con Herramientas Cobol - Cics, db2,
+                Vsam. Programador Junior en Javascript - React, Next, Node. Html
+                , Css.
               </p>
               <a href='/hireme'>Hire Me</a>
             </div>
@@ -30,9 +32,8 @@ const Index = () => (
         <div className='card  bg-light'>
           <div className='card-body'>
             <h1>Habilidades</h1>
-
             {skills.map(({ skill, percentage }, i) => (
-              <div className='py-3' jey={i}>
+              <div className='py-3' key={i}>
                 <h5>{skill}</h5>
                 <div className='progress'>
                   <div
@@ -49,7 +50,47 @@ const Index = () => (
       <div className='col-md-8'>
         <div className='card bg-light'>
           <div className='card-body'>
-            <h1>Experiencia</h1>
+            <h1>Experiencia</h1> <br />
+            <ul>
+              {experiences.map(({ title, description, from, to }, i) => (
+                <li Key={i}>
+                  <h3>{title}</h3>
+                  <h5>
+                    {from}-{to}
+                  </h5>
+                  <p>{description}</p>
+                </li>
+              ))}
+            </ul>
+            <Link href='/experiences'>
+              <a className='btn btn-light'>Ver más experiencia</a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className='row'>
+      <div className='col-md-12'>
+        <div className='card card-body bg-dark'>
+          <div className='row'>
+            <div className='col-md-12'>
+              <h1 className='text-center text-light'>Portafolio</h1>
+            </div>
+            {projects.map(({ name, description, image }, i) => (
+              <div className='col-md-4 p-2' key={i}>
+                <div className='card' h-100>
+                  <div className='overflow'>
+                    <img src={`/${image}`} alt='' className="card-img-top" />
+                  </div>
+                  <div className='card-body'>
+                    <h3>{name}</h3>
+                    <p>{description}</p>
+                    <a href='#!'>Ver Más</a>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
--- profile.js
+++ profile.js
@@ -1,30 +1,100 @@
 export const skills = [
-{
-  skill: 'Javascript',
-  percentage: 100,
-},
-{
-  skill: 'Node js',
-  percentage: 50,
-},
-{
-  skill: 'React js',
-  percentage: 70,
-},
-{
-  skill: 'Next JS',
-  percentage: 70,
-},
-{
-  skill: 'Html',
-  percentage: 80,
-},
-{
-  skill: 'Css',
-  percentage: 80,
-},
-{
-  skill: 'Cobol Main Frame',
-  percentage: 90,
-},
+  {
+    skill: 'Javascript',
+    percentage: 100
+  },
+  {
+    skill: 'Node js',
+    percentage: 50
+  },
+  {
+    skill: 'React js',
+    percentage: 70
+  },
+  {
+    skill: 'Next JS',
+    percentage: 70
+  },
+  {
+    skill: 'Html',
+    percentage: 80
+  },
+  {
+    skill: 'Css',
+    percentage: 80
+  },
+  {
+    skill: 'Cobol Main Frame',
+    percentage: 90
+  }
+]
+
+export const experiences = [
+  {
+    title: 'Sul America ING',
+    description:
+      'Analista de Sistemas / Programador, responsable del sistema de reembolso. Lider de Poryecto Reembolso Batch.',
+    from: 1999,
+    to: 2001
+  },
+  {
+    title: 'Sul America ING',
+    description:
+      'Analista de Sistemas / Programador, responsable del sistema de reembolso. Lider de Poryecto Reembolso Batch.',
+    from: 1999,
+    to: 2001
+  },
+  {
+    title: 'Sul America ING',
+    description:
+      'Analista de Sistemas / Programador, responsable del sistema de reembolso. Lider de Poryecto Reembolso Batch.',
+    from: 1999,
+    to: 2001
+  },
+  {
+    title: 'Sul America ING',
+    description:
+      'Analista de Sistemas / Programador, responsable del sistema de reembolso. Lider de Poryecto Reembolso Batch.',
+    from: 1999,
+    to: 2001
+  }
+]
+
+export const projects = [
+  {
+    name: 'Proyecto bug de Milenio',
+    description:
+      'Modificación de programas Cobol II, adecuando con (ventanas) el siglo correcto para el Banco real',
+      image: 'portfolio1.jpeg', 
+  },
+  {
+    name: 'Proyecto bug de Milenio',
+    description:
+      'Modificación de programas Cobol II, adecuando con (ventanas) el siglo correcto para el Banco real',
+      image: 'portfolio2.jpg', 
+  },
+  {
+    name: 'Proyecto bug de Milenio',
+    description:
+      'Modificación de programas Cobol II, adecuando con (ventanas) el siglo correcto para el Banco real',
+      image: 'portfolio3.png', 
+  },
+  {
+    name: 'Proyecto bug de Milenio',
+    description:
+      'Modificación de programas Cobol II, adecuando con (ventanas) el siglo correcto para el Banco real',
+      image: 'portfolio4.png', 
+  },
+  {
+    name: 'Proyecto bug de Milenio',
+    description:
+      'Modificación de programas Cobol II, adecuando con (ventanas) el siglo correcto para el Banco real',
+      image: 'portfolio5.jpeg', 
+  },
+  {
+    name: 'Proyecto bug de Milenio',
+    description:
+      'Modificación de programas Cobol II, adecuando con (ventanas) el siglo correcto para el Banco real',
+      image: 'portfolio6.jpeg', 
+  }
 ]
