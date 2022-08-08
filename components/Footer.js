4288b3ac038440bd5466f5161226ff353184ff5f
ivanugarte <ivanugartem@gmail.com>
2022-08-08 18:00:35 -0400

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -35,7 +35,7 @@ function Footer() {
                 </a>
               </Link>
               <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0 py-4 ">
-                Página confeccionada por Iván Ugarte
+                Página Hecha por Iván Andrés Ugarte Mella
               </ul>
             </div>
           </div>
