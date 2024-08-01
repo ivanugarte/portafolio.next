6394f73335f2cefe099aa9cb042125b8a8ca3cf3
Ivan Ugarte <ivanugartem@gmail.comm>
2024-08-01 00:38:01 -0400

    deploy

--- pages/form.jsx
+++ pages/form.jsx
@@ -15,7 +15,7 @@ export default function Example() {
             <div className="sm:col-span-4">
               <label htmlFor="username" className="block text-sm font-medium leading-6 text-white">
                 Username
-              </label>
+              </label>JJZZZ
               <div className="mt-2">
                 <div className="flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
                   <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
